1d53ed6638b59ee20691de9f24d71a60f57e1e7c
lm.contact <lm.contact@pm.me>
2019-07-21 18:40:03 +0200

    removing useless js file and js.map from tsc

--- src/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const encode_1 = require("./encode");
-exports.encode = encode_1.encode;
-//# sourceMappingURL=index.js.map
